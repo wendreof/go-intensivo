d3ade6f99298ed21dde3b4dfa6272af01ee93e63
wlf <wendreolf@gmail.com>
2022-11-08 20:55:10 -0300

    1st commit

---
+++ internal/order/infra/database/order_repository.go
@@ -0,0 +1,30 @@
+package database
+
+import (
+	"database/sql"
+
+	"github.com/wendreof/gointensivo/internal/order/entity"
+)
+
+type OrderRepository struct {
+	Db *sql.DB
+}
+
+func NewOrderRepository(db *sql.DB) *OrderRepository{
+	return &OrderRepository{Db: db}
+}
+
+func(r *OrderRepository) Save(order *entity.Order) error{
+	stmt, err := r.Db.Prepare("INSERT INTO ORDERS (id, price, tax, final_price) VALUES (?, ?, ?, ?) ")
+	
+	if err != nil{
+		return err
+	}
+
+	_, err = stmt.Exec(order.ID, order.Price, order.Tax, order.FinalPrice)
+	
+	if err != nil{
+		return err
+	}
+	return nil
+}

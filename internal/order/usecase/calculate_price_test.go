65f4ff59fc586cc141939a299fb3617ba0e8ef5c
wlf <wendreolf@gmail.com>
2022-11-08 21:31:08 -0300

    added interface and usecase

---
+++ internal/order/usecase/calculate_price_test.go
@@ -0,0 +1,57 @@
+package usecase
+
+import (
+	"database/sql"
+	"testing"
+
+	"github.com/stretchr/testify/suite"
+	"github.com/wendreof/gointensivo/internal/order/entity"
+	"github.com/wendreof/gointensivo/internal/order/infra/database"
+
+	//sqlite3
+	_ "github.com/mattn/go-sqlite3"
+)
+
+type CalculatePriceUseCaseTestSuite struct {
+	suite.Suite
+	OrderRepository database.OrderRepository
+	Db *sql.DB
+}
+
+func (suite *CalculatePriceUseCaseTestSuite) SetupTest(){
+	db, err := sql.Open("sqlite3", ":memory:")
+	suite.NoError(err)
+
+	_, err = db.Exec("CREATE TABLE ORDERS (id varchar(255) NOT NULL, price float NOT NULL, tax float NOT NULL, final_price float NOT NULL, PRIMARY KEY (id))")
+	suite.Db = db
+	suite.OrderRepository = *database.NewOrderRepository(db)
+}
+
+func (suite *CalculatePriceUseCaseTestSuite) TearDownTest(){
+	suite.Db.Close()
+}
+
+func TestSuite(t *testing.T){
+	suite.Run(t, new(CalculatePriceUseCaseTestSuite))
+}
+
+func (suite *CalculatePriceUseCaseTestSuite) TestCalculateFinalPrice(){
+	order, err := entity.NewOrder("1", 10, 2)
+	suite.NoError(err)
+	order.CalculatePrice()
+
+	calculateFinalPriceInput := OrderInputDTO{
+		ID: order.ID,
+		Price: order.Price,
+		Tax: order.Tax,
+	}
+
+	calculateFinalPriceUseCase := NewCalculateFinalPriceUseCase(suite.OrderRepository)
+	output, err := calculateFinalPriceUseCase.Execute(calculateFinalPriceInput)
+	suite.NoError(err)
+
+	suite.Equal(order.ID, output.ID)
+	suite.Equal(order.Price, output.Price)
+	suite.Equal(order.Tax, output.Tax)
+	suite.Equal(order.FinalPrice, output.FinalPrice)
+}

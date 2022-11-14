31b519ec81294d29870312cd9575498dec96843c
wlf <wendreolf@gmail.com>
2022-11-14 19:10:03 -0300

    Changed: cmd/consumer/main.go

--- README.md
+++ README.md
@@ -3,6 +3,12 @@
 ### Starting RabbitMQ
 `docker-compose up -d`
 
+### Creating table orders
+`sqlite3.exe orders.db`
+
+### Running consumer
+`go run cmd/consumer/main.go`
+
 ### Adding the packages
 `go mod tidy`
 
--- cmd/consumer/main.go
+++ cmd/consumer/main.go
@@ -1,24 +1,55 @@
 package main
 
 import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"time"
+
 	amqp "github.com/rabbitmq/amqp091-go"
+	"github.com/wendreof/gointensivo/internal/order/infra/database"
+	"github.com/wendreof/gointensivo/internal/order/usecase"
 	"github.com/wendreof/gointensivo/pkg/rabbitmq"
+
+	//sqlite3
+	_ "github.com/mattn/go-sqlite3"
 )
 
-//Read the msgs from a RabbitMQ queue and print it!
 func main() {
+	db, err := sql.Open("sqlite3", "./orders.db")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+	repository := database.NewOrderRepository(db)
+	uc := usecase.CalculateFinalPriceUseCase{OrderRepository: repository}
+
 	ch, err := rabbitmq.OpenChannel()
 
-	if err != nil{
+	if err != nil {
 		panic(err)
 	}
 
 	defer ch.Close()
+	out := make(chan amqp.Delivery) // channel
+	go rabbitmq.Consume(ch, out)    // T2
 
-	out := make(chan amqp.Delivery) //channel
-	go rabbitmq.Consume(ch, out) 		//Thread 2
+	for msg := range out {
+		var inputDTO usecase.OrderInputDTO
+		err := json.Unmarshal(msg.Body, &inputDTO)
 
-	for msg := range out{
-		println(string(msg.Body)) 		//Thread 1
+		if err != nil {
+			panic(err)
+		}
+
+		outputDTO, err := uc.Execute(inputDTO)
+
+		if err != nil {
+			panic(err)
+		}
+		
+		msg.Ack(false)
+		fmt.Println(outputDTO)
+		time.Sleep(500 * time.Millisecond)
 	}
 }
